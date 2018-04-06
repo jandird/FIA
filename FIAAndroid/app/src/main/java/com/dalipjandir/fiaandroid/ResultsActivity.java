af41f5262dd30375816eebcf5fd2f065a0d05e4f
kathrynkodama <kodamak@mcmaster.ca>
2018-04-05 20:01:59 -0400

    working reading and writing to online database

--- FIAAndroid/app/src/main/java/com/dalipjandir/fiaandroid/ResultsActivity.java
+++ FIAAndroid/app/src/main/java/com/dalipjandir/fiaandroid/ResultsActivity.java
@@ -2,12 +2,54 @@ package com.dalipjandir.fiaandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import org.w3c.dom.Text;
+
 
 public class ResultsActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("0/Country"); //field = test
+
+        //reading from database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override //what you want to do when data is changed
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(value);
+
+            }
+
+            @Override //what to do when you fail to read data
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+
+    public void writeToDatabase(){
+        //writing to database
+
+        myRef.setValue("hello there"); //value = hello there
+    }
+
+
+
 }
