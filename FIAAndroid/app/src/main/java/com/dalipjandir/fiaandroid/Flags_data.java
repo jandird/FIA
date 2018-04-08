8c437d98a80e0e479b7b1b910d48dcebd45cab6a
TongfeiWang <35938237+TongfeiWang@users.noreply.github.com>
2018-04-08 17:26:32 -0400

    Add files via upload

---
+++ FIAAndroid/app/src/main/java/com/dalipjandir/fiaandroid/Flags_data.java
@@ -0,0 +1,14 @@
+package com.dalipjandir.fiaandroid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jeromicho on 2018-04-08.
+ */
+
+public class Flags_data {
+    public static List<Flags> flags = new ArrayList<>();
+
+
+}
