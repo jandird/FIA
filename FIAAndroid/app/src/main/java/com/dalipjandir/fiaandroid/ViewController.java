7dfa930cc46e3a9ee6fce35253e6b656f7dcb7de
kathrynkodama <kodamak@mcmaster.ca>
2018-04-09 18:27:16 -0400

    updated more info

--- FIAAndroid/app/src/main/java/com/dalipjandir/fiaandroid/ViewController.java
+++ FIAAndroid/app/src/main/java/com/dalipjandir/fiaandroid/ViewController.java
@@ -1,6 +1,10 @@
 package com.dalipjandir.fiaandroid;
 
 import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.database.*;
+import com.google.firebase.database.ValueEventListener;
 
 public class ViewController {
 
@@ -8,6 +12,10 @@ public class ViewController {
     private static String croppedImage;
     private static Flags [] results;
     private static Flags flagResult;
+    private static FirebaseDatabase database;
+    private static DatabaseReference myRef;
+    private static int count;
+
 
     public static void setUncroppedImage(Uri input){
         uncroppedImage = input;
@@ -24,6 +32,24 @@ public class ViewController {
 
     public static void setFlag(Flags flag){
         flagResult = flag;
+        count = 0;
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        myRef.child("Countries").child(flagResult.getCountry()).child("Weight").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("test", "setFlag: " + dataSnapshot.getValue());
+                count++;
+                myRef.child("Countries").child(flagResult.getCountry()).child("Weight").setValue(count);
+
+                Log.i("test2","test: " + count);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        Log.i("test2","test: " + count);
     }
 
     public static Flags getFlag(){
